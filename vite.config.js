6c1a8124c64911a654a1f47c8f9c166ec8e75e29
Anton <aner-anton@ya.ru>
2022-12-05 23:12:27 +0300

    Add resetUserNotifications.js to vite config

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,9 @@ export default defineConfig({
     plugins: [
         laravel({
             input: [
-                'resources/css/app.css',
-                'resources/js/app.js',
+                "resources/css/app.css",
+                "resources/js/app.js",
+                "resources/js/resetUserNotifications.js",
             ],
             refresh: true,
         }),

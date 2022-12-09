3e56c1ba6054aa817854cbfa6933ba652a68f666
Anton <aner-anton@ya.ru>
2022-12-09 17:41:07 +0300

    Broadcasting works on production

--- resources/js/wsSubscriptions.js
+++ resources/js/wsSubscriptions.js
@@ -1,18 +1,16 @@
 if ($("meta[name='user_id']").length) {
     // Enable pusher logging
-    Pusher.log = function (message) {
-        if (window.console && window.console.log) window.console.log(message);
-    };
+    // Pusher.log = function (message) {
+    //     if (window.console && window.console.log) window.console.log(message);
+    // };
 
     window.Echo = new Echo({
         broadcaster: "pusher",
         key: import.meta.env.VITE_PUSHER_APP_KEY,
-        wsHost:
-            import.meta.env.VITE_PUSHER_HOST ??
-            `ws-${import.meta.env.VITE_PUSHER_APP_CLUSTER}.pusher.com`,
+        wsHost: import.meta.env.VITE_PUSHER_HOST,
         wsPort: import.meta.env.VITE_PUSHER_PORT ?? 80,
         wssPort: import.meta.env.VITE_PUSHER_PORT ?? 443,
-        forceTLS: (import.meta.env.VITE_PUSHER_SCHEME ?? "https") === "https",
+        forceTLS: true,
         enabledTransports: ["ws", "wss"],
     });
 
